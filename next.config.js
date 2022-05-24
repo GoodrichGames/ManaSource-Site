10e56d493f15664dea5d92be6972f87c83d94d6e
Paul Goodrich <pdgoodri@gmail.com>
2022-05-24 01:55:01 -0400

    Fix css and images loading, cleanup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 
 module.exports = {
   reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     loader: "custom",
   },
