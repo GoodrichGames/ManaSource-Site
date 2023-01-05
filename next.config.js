1a9b1be15f34cd13513c99548b746e077a1990cd
Paul Goodrich <pdgoodri@gmail.com>
2023-01-04 21:36:35 -0500

    Remove locale config

--- next.config.js
+++ next.config.js
@@ -8,9 +8,5 @@ module.exports = {
     loader: "custom",
     imageSizes: [640, 750, 828, 1080, 1200, 1920],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840]
-  },
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  }
 };
