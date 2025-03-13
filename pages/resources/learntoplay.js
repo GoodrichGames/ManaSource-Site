8b04ea307f1c04bdcc030634b9b1f23f3098b5ac
Paul Goodrich <pdgoodri@gmail.com>
2025-03-12 22:21:39 -0400

    Redirects testing

--- pages/resources/learntoplay.js
+++
@@ -1,6 +0,0 @@
-import BaseTemplate from './../../components/Templates/BaseTemplate';
-
-export default function LearnToPlay() {
-  return <BaseTemplate title="Resources" description="Learn to Play">
-  </BaseTemplate>
-};
